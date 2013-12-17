0403649311673cc094c353eb9dbdd376a7f4b9f8
martind <martind@mapfactor.com>
2013-12-17 15:01:53 +0100

    revised text (newer versions of applications are more robust)

--- log2html.py
+++ log2html.py
@@ -37,8 +37,8 @@ if __name__ == "__main__":
    <title>MapFactor OSM Data Processing - roundabouts report 2013.12</title>
 </head><body> <h1>MapFactor OSM Data Processing - roundabouts report
 2013.12</h1> <b>Roundabouts</b><br>MapFactor expects that roundabouts
-are closed circles. If it is not true at least navigation is confused. In the
-worse case it was even causing crashes. Here is a list of some problematic
+are closed circles. If it is not true at least navigation is confused. 
+Here is a list of some problematic
 links:<br>
 """
   for filename in sys.argv[1:]:
